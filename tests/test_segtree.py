a0061e4d79b574d673872a6f5b98ff2ba4ae3e2e
Jacob Cohen <Jacob.Cohen@aa.com>
2022-04-19 12:38:54 -0700

    added tests

---
+++ tests/test_segtree.py
@@ -0,0 +1,9 @@
+from cohentools.segtree import SegTree
+
+import math
+
+def test_segtree() -> None:
+    tree = SegTree(array := [1, 2, 3, 4, 5], min)
+    for i in range(len(array)):
+        for j in range(i, len(array) + 1):
+            assert min(array[i:j], default=math.inf) == tree.query(i, j, math.inf)
