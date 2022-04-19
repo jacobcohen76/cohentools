11dc62447ba7106b872a6eff0b8070e709dd7470
Jacob Cohen <Jacob.Cohen@aa.com>
2022-04-19 12:45:25 -0700

    added default kwarg to test_segtree

--- tests/test_segtree.py
+++ tests/test_segtree.py
@@ -6,4 +6,4 @@ def test_segtree() -> None:
     tree = SegTree(array := [1, 2, 3, 4, 5], min)
     for i in range(len(array)):
         for j in range(i, len(array) + 1):
-            assert min(array[i:j], default=math.inf) == tree.query(i, j, math.inf)
+            assert min(array[i:j], default=math.inf) == tree.query(i, j, default=math.inf)
