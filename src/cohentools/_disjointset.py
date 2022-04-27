5dc8fabddd949a79b78a5865017ebd4ab7bab42f
Jacob Cohen <jacobcohen76@protonmail.com>
2022-04-27 07:43:38 -0700

    removed trash

--- src/cohentools/_disjointset.py
+++ src/cohentools/_disjointset.py
@@ -52,17 +52,3 @@ class DisjointSet(Generic[T]):
         if not isinstance(other, DisjointSet):
             return NotImplemented
         return not self == other
-
-disjoint = DisjointSet([1, 2, 3, 4, 5, 6, 7, 8])
-
-disjoint.union(1, 2)
-disjoint.union(1, 5)
-disjoint.union(1, 6)
-disjoint.union(1, 8)
-
-disjoint.union(3, 4)
-
-disjoint.union(7, 7)
-
-print(disjoint)
-assert repr(disjoint) == "DisjointSet([{1, 2, 5, 6, 8}, {3, 4}, {7}])"
