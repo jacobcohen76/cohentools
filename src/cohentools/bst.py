25d7c4d4144638b1ae5b44a102cafa581cf53802
Jacob Cohen <jacobcohen76@protonmail.com>
2022-09-29 11:08:54 -0700

    refactor bst

--- src/cohentools/bst.py
+++ src/cohentools/bst.py
@@ -2,42 +2,44 @@ from __future__ import annotations
 
 import dataclasses
 from typing import (
-    Generic,
-    Iterable,
-    Optional,
-    TypeVar,
+  Generic,
+  Iterable,
+  TypeVar,
 )
 
 T = TypeVar("T")
 
 @dataclasses.dataclass
 class TreeNode(Generic[T]):
-    data:  T
-    left:  Optional[TreeNode[T]] = None
-    right: Optional[TreeNode[T]] = None
+  data:  T
+  left:  TreeNode[T] | None = None
+  right: TreeNode[T] | None = None
 
-def walk_preorder(root: Optional[TreeNode[T]]) -> Iterable[TreeNode[T]]:
-    stack = [root]
-    while stack:
-        if node := stack.pop():
-            yield node
-            stack.append(node.right)
-            stack.append(node.left)
+def walk_preorder(root: TreeNode[T] | None) -> Iterable[TreeNode[T]]:
+  stack = [] if root is None else [root]
+  while stack:
+    yield (node := stack.pop())
+    if node.right:
+      stack.append(node.right)
+    if node.left:
+      stack.append(node.left)
 
-def walk_inorder(root: Optional[TreeNode[T]]) -> Iterable[TreeNode[T]]:
-    stack = list[TreeNode[T]](); node = root
-    while stack or node:
-        while node:
-            stack.append(node)
-            node = node.left
-        yield (node := stack.pop())
-        node = node.right
+def walk_inorder(root: TreeNode[T] | None) -> Iterable[TreeNode[T]]:
+  stack = []; node = root
+  while stack or node:
+    while node:
+      stack.append(node)
+      node = node.left
+    yield (node := stack.pop())
+    node = node.right
 
-def walk_postorder(root: Optional[TreeNode[T]]) -> Iterable[TreeNode[T]]:
-    stack = [root]; order = []
-    while stack:
-        if node := stack.pop():
-            order.append(node)
-            stack.append(node.left)
-            stack.append(node.right)
-    return reversed(order)
+def walk_postorder(root: TreeNode[T] | None) -> Iterable[TreeNode[T]]:
+  stack = [] if root is None else [root]
+  order = []
+  while stack:
+    order.append(node := stack.pop())
+    if node.left:
+      stack.append(node.left)
+    if node.right:
+      stack.append(node.right)
+  return reversed(order)
